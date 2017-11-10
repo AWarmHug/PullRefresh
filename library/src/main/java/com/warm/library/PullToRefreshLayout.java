30ab8807cc817b1c06ef95c30a4df6e303a5fa29
Warm <865741452@qq.com>
2017-11-10 13:19:21 +0800

    初步实现。

--- library/src/main/java/com/warm/library/PullToRefreshLayout.java
+++ library/src/main/java/com/warm/library/PullToRefreshLayout.java
@@ -5,7 +5,6 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.widget.Scroller;
 
@@ -16,28 +15,30 @@ import android.widget.Scroller;
 
 public class PullToRefreshLayout extends ViewGroup {
 
-    private static final String TAG = "PullToRefreshLayout";
+    private static final String TAG = "PullToRefreshLayout--";
 
+    private View mHeader;
     private View mTarget;
 
     private int state;
 
+    private float downY;
+
     /**
-     * 模拟高度
+     * 记录不可滑动之后的滑动距离，用于给加载动画
      */
-    private int height = 200;
+    private float moveY;
+
+    /**
+     * 头部的高度
+     */
+    private int headerHeight = 200;
 
 
     /**
      * 处理滑动
      */
     private Scroller mScroller;
-
-
-    /**
-     * 判定为拖动的最小移动像素数
-     */
-    private int mTouchSlop;
 
 
     private OnRefreshListener onRefreshListener;
@@ -69,21 +70,26 @@ public class PullToRefreshLayout extends ViewGroup {
 
     public PullToRefreshLayout(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-        mScroller = new Scroller(getContext());
-        mTouchSlop = ViewConfiguration.get(getContext()).getScaledPagingTouchSlop();
+        mScroller = new Scroller(context);
 
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        ensureViews();
+
         int measureWidth = doMeasureWidth(widthMeasureSpec);
         int measureHeight = doMeasureHeight(heightMeasureSpec);
 
-        setMeasuredDimension(measureWidth, measureHeight);
-        measureChildren(widthMeasureSpec, heightMeasureSpec);
-        Log.d(TAG, "111onMeasure: ");
+        Log.d(TAG, "onMeasure: " + MeasureSpec.makeMeasureSpec(getMeasuredWidth() - getPaddingLeft() - getPaddingRight(), MeasureSpec.EXACTLY));
 
+//        mTarget.measure(MeasureSpec.makeMeasureSpec(getMeasuredWidth() - getPaddingLeft() - getPaddingRight(), MeasureSpec.EXACTLY), MeasureSpec.makeMeasureSpec(getMeasuredHeight() - getPaddingTop() - getPaddingBottom(), MeasureSpec.EXACTLY));
+//        mHeader.measure(MeasureSpec.makeMeasureSpec(getMeasuredWidth() - getPaddingLeft() - getPaddingRight(), MeasureSpec.EXACTLY), MeasureSpec.makeMeasureSpec(getMeasuredHeight() - getPaddingTop() - getPaddingBottom(), MeasureSpec.EXACTLY));
+
+        setMeasuredDimension(measureWidth, measureHeight);
+
+        measureChildren(widthMeasureSpec, heightMeasureSpec);
 
     }
 
@@ -94,9 +100,7 @@ public class PullToRefreshLayout extends ViewGroup {
         int size = MeasureSpec.getSize(measureSpec);
         switch (mode) {
             case MeasureSpec.AT_MOST:
-
-                result = getChildAt(0).getMeasuredWidth();
-                Log.d(TAG, "doMeasureWidth: " + result);
+                result = mTarget.getMeasuredWidth();
                 break;
             case MeasureSpec.EXACTLY:
                 result = size;
@@ -104,6 +108,7 @@ public class PullToRefreshLayout extends ViewGroup {
             case MeasureSpec.UNSPECIFIED:
                 break;
         }
+
         return result;
     }
 
@@ -114,8 +119,7 @@ public class PullToRefreshLayout extends ViewGroup {
         int size = MeasureSpec.getSize(measureSpec);
         switch (mode) {
             case MeasureSpec.AT_MOST:
-                result = getChildAt(0).getMeasuredHeight();
-                Log.d(TAG, "doMeasureHeight: " + result);
+                result = mHeader.getMeasuredHeight();
                 break;
             case MeasureSpec.EXACTLY:
                 result = size;
@@ -123,130 +127,108 @@ public class PullToRefreshLayout extends ViewGroup {
             case MeasureSpec.UNSPECIFIED:
                 break;
         }
+
         return result;
     }
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        int left = getPaddingLeft();
-        int top = getPaddingTop();
-//        if (changed) {
+        ensureViews();
+        if (changed) {
+            int left = getPaddingLeft();
+            int top = getPaddingTop();
 
-        top -= getChildAt(0).getMeasuredHeight();
-        height = getChildAt(0).getMeasuredHeight();
+            top -= mHeader.getMeasuredHeight();
+            headerHeight = mHeader.getMeasuredHeight();
 
-        Log.d(TAG, "onLayout: " + getChildCount());
+            Log.d(TAG, "onLayout: " + getChildCount());
 
-        for (int i = 0; i < getChildCount(); i++) {
-            View child = getChildAt(i);
-            Log.d(TAG, "onLayout: " + child);
+            for (int i = 0; i < getChildCount(); i++) {
+                View child = getChildAt(i);
+                Log.d(TAG, "onLayout: " + child);
 
-            child.layout(left, top, left + child.getMeasuredWidth(), top + child.getMeasuredHeight());
+                child.layout(left, top, left + child.getMeasuredWidth(), top + child.getMeasuredHeight());
 
-            top += child.getMeasuredHeight();
-
+                top += child.getMeasuredHeight();
+            }
         }
-//        }
     }
-
-
-    @Override
-    protected void onFinishInflate() {
-        super.onFinishInflate();
-        for (int i = 0; i < getChildCount(); i++) {
-//            if (/*getChildAt(i) instanceof RecyclerView ||*/ getChildAt(i) instanceof AbsListView || getChildAt(i) instanceof ScrollView || getChildAt(i) instanceof WebView) {
-                mTarget = getChildAt(1);
-//                break;
-//            }
-        }
-
-    }
-
-
-    private float downY;
 
     /**
-     * 记录不可滑动之后的滑动距离，用于给加载动画
+     * 是否是向下
+     *
+     * @param y
+     * @return
      */
-    private float YY;
-
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent ev) {
-        switch (ev.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                downY = ev.getY();
-                break;
-            case MotionEvent.ACTION_MOVE:
-                Log.d(TAG, "onInterceptTouchEvent: ");
-
-                if ((ev.getY() - downY) > 0 && Math.abs(ev.getY() - downY) > mTouchSlop) {
-                    if (!canChildScrollUp()) {
-                        Log.d(TAG, "onInterceptTouchEvent: 不可以上滑");
-                        return true;
-                    }
-                }
-                break;
-            case MotionEvent.ACTION_UP:
-
-                break;
-        }
-
-        return super.onInterceptTouchEvent(ev);
+    private boolean isDown(float y) {
+        return y - downY > 0;
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        switch (event.getAction()) {
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
             case MotionEvent.ACTION_DOWN:
+                if (state == State.REFRESHING) {
+                    return false;
+                }
 
                 break;
             case MotionEvent.ACTION_MOVE:
-                //处理位置
-                YY = event.getY() - downY;
-                if (getScrollY() <= 0&&mTarget.getY()>=0) {
-                    scrollTo(0, (int) -YY / 2);
-                }
-                //当滑动距离大于，最小距离（mHeader的高度）的时候，刷新，小于的时候回到顶端
-                if (-getScrollY() >= height) {
-                    //可以加载了
-                    setState(State.PUSH_OK);
+
+
+                Log.d(TAG, "dispatchTouchEvent: isDown="+isDown(ev.getY()));
+                if (isDown(ev.getY())) {
+                    if (!canChildScrollUp()) {
+                        //处理位置
+                        if (downY == 0) {
+                            downY = ev.getY();
+                        }
+                        moveY = ev.getY() - downY;
+                        Log.d(TAG, "dispatchTouchEvent: YYYY=" + moveY);
+
+                        scrollTo(0, (int) (-moveY / 2.5));
+
+                        return true;
+                    } else {
+                        downY = 0;
+                    }
                 } else {
-                    //不可以加载
-                    setState(State.PUSH_NO_OK);
+                    downY = 0;
                 }
-
-
                 break;
             case MotionEvent.ACTION_UP:
-
-
-                Log.d(TAG, "onTouchEvent: " + getScrollY());
-
+            case MotionEvent.ACTION_CANCEL:
                 //当滑动距离大于，最小距离（mHeader的高度）的时候，刷新，小于的时候回到顶端
-                if (-getScrollY() >= height) {
-
-                    //可以加载了
-                    startScroll(getScrollY(), Math.abs(getScrollY()) - height);
-                    setState(State.REFRESHING);
-                    if (onRefreshListener != null) {
-                        onRefreshListener.refresh();
+                downY = 0;
+                if (Math.abs(getScrollY()) != 0) {
+                    if (-getScrollY() >= headerHeight) {
+                        //可以加载了
+                        startScroll(getScrollY(), Math.abs(getScrollY()) - headerHeight);
+                        setState(State.REFRESHING);
+                        if (onRefreshListener != null) {
+                            onRefreshListener.refresh();
+                        }
+                    } else {
+                        //不可以加载
+                        startScroll(getScrollY(), -getScrollY());
+                        setState(State.END);
                     }
-
-
                 } else {
-                    //不可以加载
-                    startScroll(getScrollY(), -getScrollY());
                     setState(State.END);
                 }
 
                 break;
         }
-        return super.onTouchEvent(event);
+        return super.dispatchTouchEvent(ev);
+    }
+
+    private void startScroll(int y, int dy, int duration) {
+        mScroller.startScroll(0, y, 0, dy, duration);
+        invalidate();
     }
 
     private void startScroll(int y, int dy) {
-        mScroller.startScroll(0, y, 0, dy, 250);
-        invalidate();
+        startScroll(y, dy, 250);
     }
 
     @Override
@@ -256,8 +238,17 @@ public class PullToRefreshLayout extends ViewGroup {
             scrollTo(mScroller.getCurrX(), mScroller.getCurrY());
             invalidate();
         }
-        Log.d(TAG, "computeScroll1: "+mScroller.getCurrY());
+
         Log.d(TAG, "computeScroll2: " + getScrollY());
+
+        //当滑动距离大于，最小距离（mHeader的高度）的时候，刷新，小于的时候回到顶端
+        if (-getScrollY() >= headerHeight) {
+            //可以加载了
+            setState(State.PUSH_OK);
+        } else {
+            //不可以加载
+            setState(State.PUSH_NO_OK);
+        }
 
         if (onPullStateChange != null) {
             onPullStateChange.pulling(-getScrollY());
@@ -271,14 +262,14 @@ public class PullToRefreshLayout extends ViewGroup {
      * 当direction>0时，判断是否可以下滑，当direction<0时，判断是否可以上滑
      */
     public boolean canChildScrollUp() {
-        return mTarget.canScrollVertically( -1);
+        return mTarget.canScrollVertically(-1);
 
     }
 
 
     public void refreshStart() {
         //可以加载了
-        startScroll(getScrollY(), Math.abs(getScrollY()) - height);
+        startScroll(getScrollY(), Math.abs(getScrollY()) - headerHeight);
         setState(State.REFRESHING);
         if (onRefreshListener != null) {
             onRefreshListener.refresh();
@@ -318,4 +309,22 @@ public class PullToRefreshLayout extends ViewGroup {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (dipValue * scale + 0.5f);
     }
+
+
+    private void ensureViews() {
+        if (getChildCount() > 2) {
+            throw new IllegalStateException("只能有两个自控件");
+        }
+        if (mTarget == null || mHeader == null) {
+            for (int i = 0; i < getChildCount(); i++) {
+                View child = getChildAt(i);
+                if (!(child instanceof BaseHeader)) {
+                    mTarget = child;
+                } else {
+                    mHeader = child;
+                }
+            }
+        }
+    }
+
 }
